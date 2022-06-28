b0f80375de34a503e40b118217a25492b099779b
amirsmart <amirsmart1393@gmail.com>
2022-06-28 12:27:37 +0430

    hell

--- db_models/sketch.py
+++ db_models/sketch.py
@@ -67,6 +67,11 @@ def get_user_sketch_by_name(user_id,name ,engine):
     our_user = session.query(SketchModel).filter(db.and_(SketchModel.user_id == user_id  , SketchModel.name == name)).first()
     return our_user
 
+def get_user_sketch_by_token(user_id,token ,engine):
+    session = make_session(engine)
+    our_user = session.query(SketchModel).filter(db.and_(SketchModel.user_id == user_id  , SketchModel.token == token)).first()
+    return our_user
+
 def change_sketch_image(current_user: UserModel,sketch_id, url, engine):
     session = make_session(engine)
     session.query(SketchModel).filter(db.and_(SketchModel.user_id == current_user.id,SketchModel.id == sketch_id) ).update({SketchModel.image: url})
